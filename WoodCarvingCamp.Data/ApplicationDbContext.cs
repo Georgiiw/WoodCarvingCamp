3fe8da07f8282d8b5e21e9da78b2d1f5a1bcb3a8
Georgiiw <georgiivanovk@abv.bg>
2023-06-30 11:37:43 +0300

    initial commit

---
+++ WoodCarvingCamp.Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HouseRentingSystem.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WoodCarvingCamp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
